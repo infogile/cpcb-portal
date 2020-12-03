1a0132740f02c3de3ff5259531339d4586196cde
hirensavalia <hiren.savalias@gmail.com>
2020-12-04 01:13:56 +0530

    inpection-report-upload

--- components/inspection/inspection-api.js
+++ components/inspection/inspection-api.js
@@ -3,33 +3,34 @@
  * Work In Progress
  * TODO : complete the files, remove this comment
  */
-var app = require('app');
-var controller = require('./inspection-controller');
-var validator = require('./inspection-validator');
+var app = require("app");
+var controller = require("./inspection-controller");
+var validator = require("./inspection-validator");
 const router = app.getRouter();
+const helpers = require("../../helper");
 
 /* GET All Data */
-router.get('/', controller.get);
+router.get("/", controller.get);
 
 // Get All document/collection count
-router.get('/count', controller.count);
+router.get("/count", controller.count);
 
 // Findone by Id
 // router.get('/:id', controller.findOne);
 
 // Create a Inspection
-router.post('/', controller.create);
+router.post("/", controller.create);
 
 // Update a Inspection
-router.put('/:id', controller.update);
-router.put('/test-upload', controller.update);
+router.put("/:id", controller.update);
+router.put("/test-upload", controller.update);
 
 // Delete a Inspection
-router.delete('/:id', controller.delete);
+router.delete("/:id", controller.delete);
 // my status
-router.get('/mystatus', controller.myStatus);
+router.get("/mystatus", controller.myStatus);
 // my Inspection
-router.post('/myinspection', controller.myInspection);
+router.post("/myinspection", controller.myInspection);
 // my active inspection
 router.post("/myactiveinspection", controller.myActiveInspection);
 //all my active inspection
@@ -37,5 +38,17 @@ router.post("/myallinspection", controller.myAllInspection);
 //get field report
 router.get("/getfieldreport/:report_id", controller.getFieldReport);
 // my field report
-router.post('/myfieldReport', controller.myFieldReport);
+router.post("/myfieldReport", controller.myFieldReport);
+//upload consent copy
+router.post(
+  "/consentcopy",
+  helpers.uploadReport.single("consentcopy"),
+  controller.uploadReport
+);
+//upload inspection report
+router.post(
+  "/inspectionreport",
+  helpers.uploadReport.single("inspectionreport"),
+  controller.uploadReport
+);
 module.exports = router;
--- components/inspection/inspection.js
+++ components/inspection/inspection.js
@@ -1,171 +1,207 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const InspectionSchema = new mongoose.Schema(
-    {
-        factory: { type: mongoose.Schema.Types.ObjectId, ref: 'factory', required: true, trim: true },
-        assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true, trim: true },
-        status: { type: Number, default: 0 }, // 0 - Assigned | 1 - Inspected | 2 - Completed | 3 - SOS
-        attendance: {
-            entrylocation: { type: { type: String }, coordinates: [String] },
-            exitlocation: { type: { type: String }, coordinates: [String] },
-        },
-        fieldReport: {
-            images: [String],
-            poc: [
-                // contacted person
-                {
-                    name: { type: String, lowercase: true, trim: true },
-                    number: { type: String, lowercase: true, trim: true },
-                    email: { type: String, lowercase: true, trim: true },
-                },
-            ],
-            uos: {
-                // Unit operational status
-                type: String, // 0 - Non operational | 1 - Operational | 2 - other
-            },
-            uosdetail: {
-                // unit operational status 2  then add information
-                type: String,
-            },
-            nous: {
-                // Non operational unit status
-                type: String, // 0 - Temporary closed | 1 - Self closed | 2 - others
-            },
-            nousdetail: {
-                // non operation type 2 then add information
-                type: String,
-            },
-            etpos: {
-                // ETP operational status
-                type: String, // 0 - Non operational | 1 - Operational | 2 - other
-            },
-            etposdetail: { type: String },
-            cpc: {
-                // consented production capacity
-                type: String,
-            },
-            ipc: {
-                // installed production capacity
-                type: String,
-            },
-            ppopd: {
-                //present production of previous day
-                type: String,
-            },
-            fwwpdbofm: {
-                //fresh water withdrawal previous day based on flow meter
-                type: String,
-            },
-            ocs: {
-                // online connectivity status
-                type: String, // 0 - Not Connected | 1 - Connected
-            },
-            sonfc: {
-                // Status of noc from cgwa
-                type: String, // 0 - Not Valid | 1 - Valid
-            },
-            mrr: {
-                // Mode to reach river
-                type: String, // 0 - Drain | 1 - Tributary | 2 - Directly to river ganga
-            },
-            mrrname: {
-                // drain or tributary name
-                type: String,
-            },
-            csac: {
-                // consent status air consent
-                type: String, // 0 - Invalid | 1 - Valid | 2 - Applied | 3 - Not Provided
-            },
-            wc: {
-                // water consent
-                type: String, // 0 - Invalid | 1 - Valid | 2 - Applied | 3 - Not Provided
-            },
-            hc: {
-                // Hazardouc consent
-                type: String, // 0 - Invalid | 1 - Valid | 2 - Applied | 3 - Not Provided
-            },
-            cc: {
-                // CGWA consent
-                type: String, // 0 - Invalid | 1 - Valid | 2 - Applied | 3 - Not Provided
-            },
-            sfwc: {
-                // Source of fresh water consumption
-                type: String, // 0 - Borewell | 1 - Tubewell | 2 - River | 3 - Other
-            },
-            sfwcdetail: {
-                // fresh water source 3 then explain or give reason
-                type: String,
-            },
-            fib: {
-                // Flowmeter installed at Borewell
-                type: String, // 0 - Installed & working | 1 - Installed & not working | 2 - Not Installed | 3 - Other
-            },
-            fibdetail: {
-                // Flowmeter installed at Borewell detail
-                type: String,
-            },
-            fietpinlet: {
-                // Flowmeter installed at Etp inlet
-                type: String, // 0 - Installed & working | 1 - Installed & not working | 2 - Not Installed | 3 - Other
-            },
-            fietpinletdetail: {
-                // Flowmeter installed at Etp inlet detail
-                type: String,
-            },
-            fietpoutlent: {
-                // Flowmeter installed at Etp outlent
-                type: String, // 0 - Installed & working | 1 - Installed & not working | 2 - Not Installed | 3 - Other
-            },
-            fietpoutlentdetail: {
-                // Flowmeter installed at Etp outlent detail
-                type: String,
-            },
-            fmetpoutletcdf: {
-                // Flow meter at etp outlet current day flow (KLD)
-                type: String,
-            },
-            fmetpoutletpdf: {
-                // Flow meter at etp outlet Previous day flow (KLD)
-                type: String,
-            },
-            os: {
-                // ocems status
-                type: String, // 0 - Installed & working | 1 - Installed & not working | 2 - Not Installed | 3 - Other
-            },
-            osdetail: {
-                // ocems status detail
-                type: String,
-            },
-            semfetp: {
-                // Separate energy for etp
-                type: String,
-            },
-            semfer: {
-                // Separate energy for etp reading
-                type: String,
-            },
-            specificobservations: {
-                // Specific Observations
-                type: String,
-            },
-        },
-        report: {
-            files: [String],
-            images: [String],
-        },
-        actions: [
-            { title: String, description: String, createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } },
-        ],
-        sos: {
-            type: String,
-        },
-        sosdetails: {
-            type: String,
-        },
+  {
+    factory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "factory",
+      required: true,
+      trim: true,
     },
-    {
-        timestamps: true,
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+      trim: true,
     },
+    status: { type: Number, default: 0 }, // 0 - Assigned | 1 - Inspected | 2 - Completed | 3 - SOS
+    attendance: {
+      entrylocation: { type: { type: String }, coordinates: [String] },
+      exitlocation: { type: { type: String }, coordinates: [String] },
+    },
+    teamNames: { type: String, trim: true }, // Memeber of inspection Team
+    finalRecommendation: { type: String, trim: true }, // Final Recommendation
+    complianceStatus: { type: Boolean }, // Compliance status as per discharge norms
+    wasteWaterGeneration: { type: String, trim: true }, // Waste water generation
+    wasteWaterDischarge: { type: String, trim: true }, // Waste water discharge
+    bod: { type: String, trim: true }, // BOD
+    bodLoad: { type: String, trim: true }, // BOD Load
+    cod: { type: String, trim: true }, // COD
+    codLoad: { type: String, trim: true }, // COD Load
+    otherChars: { type: String, trim: true }, // Other characterestics
+    nonInstallationofOCEMS: { type: Boolean, default: false }, // Non-installation and non-connectivity of OCEMS
+    temperedOCEMS: { type: Boolean, default: false }, // OCEMS found disconnected/partial OCEMS/tampered.
+    dissentBypassArrangement: { type: Boolean, default: false }, // More than consented outlet(s) and/or any bypass arrangement
+    provision: { type: Boolean, default: false }, // Provision for flow diversion through flexible pipelines in the ETP
+    defunctETP: { type: Boolean, default: false }, // Defunct/ non-operational ETP which could not be made operational during inspection
+    ZLDnorms: { type: Boolean, default: false }, // Non-compliance of ZLD norms (in case of ZLD units)
+    standardExceedance: { type: Boolean, default: false }, // Exceedance of the notified effluent discharge standards
+    dilutionInETP: { type: Boolean, default: false }, // Dilution or addition of fresh water at any stage of treatment in ETP.
+    dissentWaterDischarge: { type: Boolean, default: false }, // Waste water discharge beyond consented quantity.
+    unauthorizedDisposal: { type: Boolean, default: false }, // Unauthorized disposal of Hazardous waste or inadequate Hazardous Waste storage capacity
+    effluent: { type: Boolean, default: false }, // Injection of treated or untreated effluent or both into ground water (reverse boring).
+    invalidCTO: { type: Boolean, default: false }, // Invalid CTO, HW authorization (wherever applicable) & not applied for fresh/renewal.
+    fieldReport: {
+      images: [String],
+      poc: [
+        // contacted person
+        {
+          name: { type: String, lowercase: true, trim: true },
+          number: { type: String, lowercase: true, trim: true },
+          email: { type: String, lowercase: true, trim: true },
+        },
+      ],
+      uos: {
+        // Unit operational status
+        type: String, // 0 - Non operational | 1 - Operational | 2 - other
+      },
+      uosdetail: {
+        // unit operational status 2  then add information
+        type: String,
+      },
+      nous: {
+        // Non operational unit status
+        type: String, // 0 - Temporary closed | 1 - Self closed | 2 - others
+      },
+      nousdetail: {
+        // non operation type 2 then add information
+        type: String,
+      },
+      etpos: {
+        // ETP operational status
+        type: String, // 0 - Non operational | 1 - Operational | 2 - other
+      },
+      etposdetail: { type: String },
+      cpc: {
+        // consented production capacity
+        type: String,
+      },
+      ipc: {
+        // installed production capacity
+        type: String,
+      },
+      ppopd: {
+        //present production of previous day
+        type: String,
+      },
+      fwwpdbofm: {
+        //fresh water withdrawal previous day based on flow meter
+        type: String,
+      },
+      ocs: {
+        // online connectivity status
+        type: String, // 0 - Not Connected | 1 - Connected
+      },
+      sonfc: {
+        // Status of noc from cgwa
+        type: String, // 0 - Not Valid | 1 - Valid
+      },
+      mrr: {
+        // Mode to reach river
+        type: String, // 0 - Drain | 1 - Tributary | 2 - Directly to river ganga
+      },
+      mrrname: {
+        // drain or tributary name
+        type: String,
+      },
+      csac: {
+        // consent status air consent
+        type: String, // 0 - Invalid | 1 - Valid | 2 - Applied | 3 - Not Provided
+      },
+      wc: {
+        // water consent
+        type: String, // 0 - Invalid | 1 - Valid | 2 - Applied | 3 - Not Provided
+      },
+      hc: {
+        // Hazardouc consent
+        type: String, // 0 - Invalid | 1 - Valid | 2 - Applied | 3 - Not Provided
+      },
+      cc: {
+        // CGWA consent
+        type: String, // 0 - Invalid | 1 - Valid | 2 - Applied | 3 - Not Provided
+      },
+      sfwc: {
+        // Source of fresh water consumption
+        type: String, // 0 - Borewell | 1 - Tubewell | 2 - River | 3 - Other
+      },
+      sfwcdetail: {
+        // fresh water source 3 then explain or give reason
+        type: String,
+      },
+      fib: {
+        // Flowmeter installed at Borewell
+        type: String, // 0 - Installed & working | 1 - Installed & not working | 2 - Not Installed | 3 - Other
+      },
+      fibdetail: {
+        // Flowmeter installed at Borewell detail
+        type: String,
+      },
+      fietpinlet: {
+        // Flowmeter installed at Etp inlet
+        type: String, // 0 - Installed & working | 1 - Installed & not working | 2 - Not Installed | 3 - Other
+      },
+      fietpinletdetail: {
+        // Flowmeter installed at Etp inlet detail
+        type: String,
+      },
+      fietpoutlent: {
+        // Flowmeter installed at Etp outlent
+        type: String, // 0 - Installed & working | 1 - Installed & not working | 2 - Not Installed | 3 - Other
+      },
+      fietpoutlentdetail: {
+        // Flowmeter installed at Etp outlent detail
+        type: String,
+      },
+      fmetpoutletcdf: {
+        // Flow meter at etp outlet current day flow (KLD)
+        type: String,
+      },
+      fmetpoutletpdf: {
+        // Flow meter at etp outlet Previous day flow (KLD)
+        type: String,
+      },
+      os: {
+        // ocems status
+        type: String, // 0 - Installed & working | 1 - Installed & not working | 2 - Not Installed | 3 - Other
+      },
+      osdetail: {
+        // ocems status detail
+        type: String,
+      },
+      semfetp: {
+        // Separate energy for etp
+        type: String,
+      },
+      semfer: {
+        // Separate energy for etp reading
+        type: String,
+      },
+      specificobservations: {
+        // Specific Observations
+        type: String,
+      },
+    },
+    report: {
+      files: [String],
+      images: [String],
+    },
+    actions: [
+      {
+        title: String,
+        description: String,
+        createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      },
+    ],
+    sos: {
+      type: String,
+    },
+    sosdetails: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model('inspection', InspectionSchema);
+module.exports = mongoose.model("inspection", InspectionSchema);
