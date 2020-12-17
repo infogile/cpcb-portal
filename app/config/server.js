d99fb98703773081843971406da4cbd929456e9f
hirensavalia <hiren.savalias@gmail.com>
2020-12-18 03:41:50 +0530

    https

--- app/config/server.js
+++ app/config/server.js
@@ -1,12 +1,13 @@
 module.exports = {
-  env: process.env.NODE_ENV || 'development',
-  protocol: 'http', // http or https
-  host: '0.0.0.0',
+  env: process.env.NODE_ENV || "development",
+  protocol: process.env.NODE_ENV === "production" ? "https" : "http", // http or https
+  host: "0.0.0.0",
   port: process.env.PORT || 9090,
 
   // TODO update this for https
   certificates: {
-    // key: fs.readFileSync('/home/denma/.ssl/certs/localhost.key'),
-    // cet: fs.readFileSync('/home/denma/.ssl/certs/localhost.crt')
-  }
-}
+    key: fs.readFileSync("/home/dropletadmin/.ssl/privkey.pem", "utf8"),
+    cert: fs.readFileSync("/home/dropletadmin/.ssl/cert.pem", "utf8"),
+    ca: fs.readFileSync("/home/dropletadmin/.ssl/chain.pem", "utf8"),
+  },
+};
