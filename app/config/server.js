8fb8218110b30bd54c2c9d072a3d83a425e94244
1496-rajeev <1496piku@gmail.com>
2021-03-18 12:01:09 +0530

    added ssl certificate

--- app/config/server.js
+++ app/config/server.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 
 module.exports = {
   env: process.env.NODE_ENV || "development",
-  protocol: process.env.NODE_ENV === "production" ? "http" : "http", // http or https
+  protocol: process.env.NODE_ENV === "production" ? "https" : "http", // http or https
   host: "0.0.0.0",
   port: process.env.PORT || 9090,
 
   // TODO update this for https
-  // certificates: {
-  //   key: fs.readFileSync("/home/dropletadmin/.ssl/privkey.pem", "utf8"),
-  //   cert: fs.readFileSync("/home/dropletadmin/.ssl/cert.pem", "utf8"),
-  //   ca: fs.readFileSync("/home/dropletadmin/.ssl/chain.pem", "utf8"),
-  // },
+  certificates: {
+    key: fs.readFileSync("/home/dropletadmin/.ssl/privkey.pem", "utf8"),
+    cert: fs.readFileSync("/home/dropletadmin/.ssl/cert.pem", "utf8"),
+    ca: fs.readFileSync("/home/dropletadmin/.ssl/chain.pem", "utf8"),
+  },
 };
